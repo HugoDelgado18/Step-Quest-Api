3beba567985e396328fe4ecd629f5b93a1f612a7
hugo delgado <hugo.delgado1@outlook.com>
2024-02-20 21:15:28 -0700

    starter code

---
+++ src/main/java/com/StepQuest/StepQuest/StepQuestApplication.java
@@ -0,0 +1,13 @@
+package com.StepQuest.StepQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StepQuest/StepQuest/StepQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StepQuest.StepQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
